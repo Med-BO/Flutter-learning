9a09356abeb079e2c16826b22f44882b12682309
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2022-12-10 13:38:07 +0100

    Developed a dice rolling app

---
+++ Dice_app/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 6;
+  int rightDiceNumber = 6;
+
+  rollDices() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                child: Image.asset("images/dice$leftDiceNumber.png"),
+                onPressed: () {
+                  rollDices();
+                },
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: TextButton(
+                child: Image.asset("images/dice$rightDiceNumber.png"),
+                onPressed: () {
+                  rollDices();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
